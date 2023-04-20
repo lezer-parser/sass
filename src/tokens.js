221ef92ca0ae059fcbbb2309ae82032aa04c2bfc
Marijn Haverbeke <marijn@haverbeke.berlin>
2023-04-20 11:42:33 +0200

    Implement SCSS grammar

--- src/tokens.js
+++ src/tokens.js
@@ -2,12 +2,13 @@
    expressed by Lezer's built-in tokenizer. */
 
 import {ExternalTokenizer} from "@lezer/lr"
-import {callee, identifier, VariableName, descendantOp, Unit} from "./parser.terms.js"
+import {callee, identifier, VariableName, descendantOp, Unit,
+        InterpolationStart, InterpolationEnd, InterpolationContinue} from "./parser.terms.js"
 
 const space = [9, 10, 11, 12, 13, 32, 133, 160, 5760, 8192, 8193, 8194, 8195, 8196, 8197,
                8198, 8199, 8200, 8201, 8202, 8232, 8233, 8239, 8287, 12288]
 const colon = 58, parenL = 40, underscore = 95, bracketL = 91, dash = 45, period = 46,
-      hash = 35, percent = 37
+      hash = 35, percent = 37, braceL = 123, braceR = 125
 
 function isAlpha(ch) { return ch >= 65 && ch <= 90 || ch >= 97 && ch <= 122 || ch >= 161 }
 
@@ -20,11 +21,26 @@ export const identifiers = new ExternalTokenizer((input, stack) => {
       if (!inside && (next != dash || i > 0)) inside = true
       if (dashes === i && next == dash) dashes++
       input.advance()
+    } else if (next == hash && input.peek(1) == braceL) {
+      input.acceptToken(InterpolationStart, 2)
+      break
     } else {
       if (inside)
         input.acceptToken(next == parenL ? callee : dashes == 2 && stack.canShift(VariableName) ? VariableName : identifier)
       break
     }
+  }
+})
+
+export const interpolationEnd = new ExternalTokenizer(input => {
+  if (input.next == braceR) {
+    input.advance()
+    while (isAlpha(input.next) || input.next == dash || input.next == underscore || isDigit(input.next))
+      input.advance()
+    if (input.next == hash && input.peek(1) == braceL)
+      input.acceptToken(InterpolationContinue, 2)
+    else
+      input.acceptToken(InterpolationEnd)
   }
 })
 
